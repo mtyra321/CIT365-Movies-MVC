e95f890a47ba2534c83dea1bbb16890bff9eb7eb
mtyra321 <mtyra321@gmail.com>
2021-11-06 19:16:28 -0600

    Add project files.

---
+++ Movies MVC/Data/Movies_MVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Movies_MVC.Models;
+
+namespace Movies_MVC.Data
+{
+    public class Movies_MVCContext : DbContext
+    {
+        public Movies_MVCContext (DbContextOptions<Movies_MVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movies_MVC.Models.Movie> Movie { get; set; }
+    }
+}
